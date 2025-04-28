527b936dfbb5aa193bc4766d8c8cc06df72cdd87
LEE I-CHENG <>
2025-04-29 00:04:23 +0800

    Feat: use compute_pose in Pose head

--- ultralytics/nn/modules/head.py
+++ ultralytics/nn/modules/head.py
@@ -242,26 +242,23 @@ class OBB(Detect):
 class Pose(Detect):
     """YOLO Pose head for keypoints models."""
 
-    def __init__(self, nc=80, kpt_shape=(17, 3), ch=()):
+    def __init__(self, nc=80, kpt_shape=(17, 3), ch=(),compute_pose:bool=True):
         """Initialize YOLO network with default parameters and Convolutional Layers."""
         super().__init__(nc, ch)
+        self.compute_pose = compute_pose
         self.kpt_shape = kpt_shape  # number of keypoints, number of dims (2 for x,y or 3 for x,y,visible)
         self.nk = kpt_shape[0] * kpt_shape[1]  # number of keypoints total
-
         c4 = max(ch[0] // 4, self.nk)
         self.cv4 = nn.ModuleList(nn.Sequential(Conv(x, c4, 3), Conv(c4, c4, 3), nn.Conv2d(c4, self.nk, 1)) for x in ch)
 
-    def forward(self, x):
+    def forward(self, x,compute_pose:bool=True):
         """Perform forward pass through YOLO model and return predictions."""
-        usePose = True
-
         if self.training:
             bs = x[0].shape[0]  # batch size
             kpt = torch.cat([self.cv4[i](x[i]).view(bs, self.nk, -1) for i in range(self.nl)], -1)  # (bs, 17*3, h*w)
             y = Detect.forward(self, x)
             return y, kpt
-
-        if usePose:
+        if compute_pose :
             bs = x[0].shape[0]  # batch size
             kpt = torch.cat([self.cv4[i](x[i]).view(bs, self.nk, -1) for i in range(self.nl)], -1)  # (bs, 17*3, h*w)
             y = Detect.forward(self, x)
